a3e9dacd269abaf9642758a589f44cdae3c213e3
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-03-05 08:01:38 -0500

    Added Random Number
    Added the random number and the Scanner keyboard for user input.

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class NumberGuessing {
+
+
+
+    public static void main (String[]args){
+
+        //Declare
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int randomNumber = (int)(Math.random()*100);
+        int userNumber;
+
+        //Ask
+        System.out.println();
+    }
 }
+
