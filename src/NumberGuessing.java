bf63f3b0ffe9d921f1f8d081548bfb1c4a962c93
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-03-07 08:10:11 -0500

    Fixed Loops
    Loops did not run as desired, so I changed them and added some "If" and "If else" statements.

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -17,19 +17,21 @@ public class NumberGuessing {
         System.out.println("Guess a whole number between 0 and 100.");
         userNumber = keyboard.nextInt();
 
-        while (userNumber < randomNumber) {
-            System.out.println("Too low! Try again.");
-            userNumber = keyboard.nextInt();
-        }
-        while (userNumber > randomNumber) {
-            System.out.println("Too high! Try again!");
-            userNumber = keyboard.nextInt();
 
+        while(userNumber!=randomNumber){
+            if(userNumber>randomNumber){
+              System.out.println("Too high! Try again.");
+              userNumber=keyboard.nextInt();
+            }
+            else if (userNumber<randomNumber){
+                System.out.println("Too low! Try again");
+                userNumber=keyboard.nextInt();
+            }
         }
-        System.out.println("Congradulations, you chose the correct number!");
+
+
         System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+        System.out.println("Congratulations, you chose the correct number!");
         System.out.println(" ");
         System.out.println(" ");
         System.out.println("Just so you know this ran correctly, your number was " +userNumber + " and the randomly generated " +
