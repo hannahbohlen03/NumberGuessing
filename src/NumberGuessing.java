9c4019cbbf1554c57e10c139daf2070ad5707d7d
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-03-07 08:27:58 -0500

    Fixing Formating

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -5,31 +5,31 @@ public class NumberGuessing {
 
     public static void main(String[] args) {
 
-        //Declare
+        //Declare Some Stuff
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         int randomNumber = (int) (Math.random() * 100);
         int userNumber;
 
 
-        //Ask
+        //Ask User
         System.out.println("Hello, there! Let's play a guessing game . . . ");
         System.out.println("Guess a whole number between 0 and 100.");
         userNumber = keyboard.nextInt();
 
 
+        //Actual Loops Portions
         while(userNumber!=randomNumber){
             if(userNumber>randomNumber){
-              System.out.println("Too high! Try again.");
-              userNumber=keyboard.nextInt();
+                System.out.println("Too high! Try again.");
+                userNumber=keyboard.nextInt();
             }
             else if (userNumber<randomNumber){
                 System.out.println("Too low! Try again");
                 userNumber=keyboard.nextInt();
             }
         }
-
-
+        //You did it! Yay!
         System.out.println(" ");
         System.out.println("Congratulations, you chose the correct number!");
         System.out.println(" ");
